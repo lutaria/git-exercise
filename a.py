d26206c82b89e51d7e50c50a86606c3fbe2105ce
Jasper Wang <47802119+lutaria@users.noreply.github.com>
2023-12-27 20:09:09 +0800

    impl py (#4)

---
+++ a.py
@@ -0,0 +1,21 @@
+def hello():
+    print(hello)
+
+
+def world():
+    print(world)
+
+
+def foo():
+    print("foo")
+
+
+def bar():
+    print("bar")
+
+
+if __name__ == '__main__':
+    hello()
+    world()
+    foo()
+    bar()
