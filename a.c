926a1ea14ae5aa771e2b206e2914a28493957992
Jasper Wang <47802119+lutaria@users.noreply.github.com>
2023-12-27 19:53:15 +0800

    Feature1 (#1)

--- a.c
+++ a.c
@@ -4,9 +4,20 @@ void hello() {
     printf("hello\n");
 }
 
+void foo() {
+    printf("foo\n");
+}
+
+void bar() {
+    printf("bar\n");
+}
+
 int main() {
 
     hello();
+    foo();
+    bar();
 
     return 0;
 }
+
